e2c28af3b6dfd2098d85708116634414e96feaf6
Gustavo Age <ageg590@gmail.com>
2025-11-03 09:02:13 -0300

    feat: create data mock

---
+++ BreakingNews/src/Datas.js
@@ -0,0 +1,23 @@
+export const news = [
+   {
+        title: 'Noticia 1',
+        content: 'Lorem Ipsum',
+        image: 'https://image-example.com/',
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: 'Noticia 2',
+        content: 'Lorem Ipsum',
+        image: 'https://image-example.com/',
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: 'Noticia 3',
+        content: 'Lorem Ipsum',
+        image: 'https://image-example.com/',
+        likes: 2,
+        comments: 4
+    }
+]
