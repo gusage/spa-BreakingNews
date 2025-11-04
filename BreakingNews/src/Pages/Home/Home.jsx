7567f8538fe3e56a68bd1ab55973785a9bdf2e5d
Gustavo Age <ageg590@gmail.com>
2025-11-04 17:17:11 -0300

    feat: create component navbar, add booststrap icons

---
+++ BreakingNews/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from '../../Components/Navbar/Navbar.jsx'
+
+export default function Home() {
+    return (
+        <section>
+            <Navbar />
+            <h1>Ola Home</h1>
+        </section>
+    )
+}
