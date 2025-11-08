f0d5d6201ce2146e21d9343f9dcac25270e182f3
Gustavo Age <ageg590@gmail.com>
2025-11-07 22:34:04 -0300

    feat: added function to find the posts from the backend in the frontend

--- BreakingNews/src/Pages/Home/Home.jsx
+++ BreakingNews/src/Pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import { Card } from '../../Components/Card/Card.jsx'
 import { Navbar } from '../../Components/Navbar/Navbar.jsx'
 import { news } from '../../Datas.jsx'
+import { getAllNews } from '../../services/newsService.js';
 import { HomeBody } from './HomeStyled.jsx';
 
 export default function Home() {
+
+    async function findAllNews() {
+        const response = await getAllNews();
+        console.log(response.data.results);
+    };
+
+    findAllNews();
+
     return (
         <>
             <Navbar />
